5dee2aac4f73c8e5b73440f75d4fa7c272904691
Troy Chryssos <github email address>
2016-08-03 16:54:18 -0400

    Complete bear lab Wrote SQL prompts to create a table of bears, and then populated it with bears, and then searched those bears.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 3, "M", "Brown", "Kind", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 9, "M", "Gold", "Rowdy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 17, "F", "Black", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 35, "M", "White", "Stern", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 6, "F", "Tan", "Rambunctious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 18, "M", "Green", "Grumpy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 13, "F", "Brown", "High-Strung", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 100, "M", "Translucent", "Spiritual", 0);
